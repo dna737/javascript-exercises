a21410b138e80f9dce772aab1bf4ef14d5fe5405
dna737 <adnanhere27@gmail.com>
2022-07-13 23:58:10 +0530

    passed all 6 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = (value) => {
+  return Math.round(((value-32)/9) * 5 * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = (value) => {
+  return Math.round(((9/5) * value + 32) * 10)/10;
 };
 
 // Do not edit below this line
