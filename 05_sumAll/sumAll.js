f52868ec9758df740a0dac70a330edf6c45736e8
dna737 <adnanhere27@gmail.com>
2022-07-13 12:03:14 +0530

    passed 6 tests for 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = (start, finish) => {
+    if(!Number.isInteger(start) || !Number.isInteger(finish)){
+        return "ERROR";
+    }
 
+    if(start < 0 || finish < 0){
+        return "ERROR";
+    }
+    //If start is greater than finish, we just have to swap the start and finish values since addition is commutative.
+    if(start > finish){
+        let temp = start;
+        start = finish;
+        finish = temp;
+    }
+    let sum = 0;
+    for(let i = start; i <= finish; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
