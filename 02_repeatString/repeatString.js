d8a128fda12264e1b4c499c5bd77bc4dc4930deb
dna737 <adnanhere27@gmail.com>
2022-07-13 06:24:27 +0530

    passed all 7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = (string, number) => {
+    if(number < 0){
+        return "ERROR";
+    }
+    let answer = "";
+    for(let i = 0; i < number; i++){
+        answer += string;
+    }
+    return answer;
 };
 
 // Do not edit below this line
