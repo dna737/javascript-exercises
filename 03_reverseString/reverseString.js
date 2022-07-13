9afa4b038f6e53b4829edca05fe8574d5a1624c1
dna737 <adnanhere27@gmail.com>
2022-07-13 06:33:17 +0530

    pass all 4 tests in 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    let reverseString = "";
+    for(let i = 0; i < string.length; i++){
+        reverseString += string.charAt(string.length-1-i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
