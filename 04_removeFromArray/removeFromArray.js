6e419c62efe4d0820034f3cb935871a770298be4
dna737 <adnanhere27@gmail.com>
2022-07-13 11:42:30 +0530

    passed all 7 tests in 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = (array, ...removeables) => {    
+        for(let j = 0; j < removeables.length; j++){
+            for(let i = 0; i < array.length; i++){
+            if(array[i] === removeables[j]){
+                array.splice(i, 1);
+            }
+        }
+        }
+        return array;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
