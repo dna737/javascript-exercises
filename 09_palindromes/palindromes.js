ef449f4202d3b585540fcdc2184ac6d177980b8c
dna737 <adnanhere27@gmail.com>
2022-07-27 20:31:21 +0530

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = (string) => {
+    let newString = string.toLowerCase();
+    //reversing the string:
+    let reverse = "";
+    for(let i = newString.length - 1; i >= 0; i--){
+        if(isAlphabet(newString.charAt(i))){
+            reverse += newString.charAt(i);
+        }else{
+            //remove that punctuation/whitespace(?):
+            newString = newString.replace(newString.charAt(i), "");
+        }
+    }
 
+    return newString === reverse;
+
+    //removing the punctuations from the og string:
 };
+
+const isAlphabet = (character) => {
+    return (character.length === 1 && character >= 'a' && character <= 'z');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
